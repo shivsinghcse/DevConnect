d44bdb154bb7e1c6df54d00b246abb9c355ab7a1
shivsinghcse <shivsinghcse@gmail.com>
2026-03-19 17:18:08 +0530

    feat: improve navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,36 +24,37 @@ const Navbar = () => {
 
     return (
         <div className="navbar bg-base-300 shadow-sm">
-            <div className="flex-1">
-                <Link to={'/'} className="btn btn-ghost text-xl">Dev🔗Connect</Link>
+            <div className="flex-1 ml-4">
+                <Link to={'/'} className="text-2xl font-bold">Dev🔗Connect</Link>
             </div>
             <div className="flex gap-2">
-                <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
                 {
                     user && (
-                        <div className="dropdown dropdown-end">
+                        <div className="dropdown dropdown-end flex justify-center items-center gap-4  mr-4">
                             <p>Welcome, {user.firstName}</p>
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src={user ? user.photoUrl : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} 
-                            />
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="Tailwind CSS Navbar component"
+                                        src={user ? user.photoUrl : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} 
+                                    />
+                                </div>
+                            </div>
+                            <ul
+                                tabIndex="-1"
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-50 w-52 p-2 shadow">
+                                <li className="text-center border-b pb-2 mb-2 ">Welcome, {user.firstName}</li>
+                                <li>
+                                <Link to={'/profile'} className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </Link>
+                                </li>
+                                <li><a>Settings</a></li>
+                                <li><Link onClick={handleLogout}>Logout</Link></li>
+                            </ul>
                         </div>
-                    </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li>
-                        <Link to={'/profile'} className="justify-between">
-                            Profile
-                            <span className="badge">New</span>
-                        </Link>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><Link onClick={handleLogout}>Logout</Link></li>
-                    </ul>
-                </div>
                     )
                 }
             </div>
