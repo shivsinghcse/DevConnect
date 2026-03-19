b1e0f18ce9401fa589597fbd8acaac57bd5d5bfe
shivsinghcse <shivsinghcse@gmail.com>
2026-03-19 17:38:23 +0530

    feat: improve userCard design

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,18 +5,19 @@ const UserCard = ({user}) => {
     return (
     <div className="flex justify-center items-center h-[80vh]">
         <div className="card bg-base-300 w-96 shadow-sm">
-            <figure>
+            <figure className=" w-96 aspect-square">
                 <img
                 src={photoUrl ?? "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"}
-                alt="Shoes" />
+                alt="Shoes"
+                className="w-full object-cover" />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{`${firstName} ${lastName}`}</h2>
-                {age && gender && <p>{`${age}, ${gender}`}</p>}
+                {age && gender && <p className="capitalize">{`${age}, ${gender}`}</p>}
                 <p>{about}</p>
                 <div className="card-actions justify-center gap-8 mt-4">
-                <button className="btn btn-primary">Ignore</button>
-                <button className="btn btn-secondary">Send Request</button>
+                <button className="btn btn-primary py-2 px-12 ">Ignore</button>
+                <button className="btn btn-secondary py-2 px-8">Send Request</button>
                 </div>
             </div>
         </div>
